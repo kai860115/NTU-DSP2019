57c8ef62fc64e2146aae32d1ac2ecd34b9529aec
kai860115 <37171138+kai860115@users.noreply.github.com>
2020-05-13 04:19:43 +0800

    Delete get_data.sh

--- dsp_final/get_data.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-wget -O speechdata.zip https://www.dropbox.com/s/bvdsk1pdq1cb5i5/speechdata.zip?dl=1
-wget -O hw2_1.zip https://www.dropbox.com/s/0obx5riquh9brb9/dsp_hw2_1.zip?dl=1
-unzip speechdata.zip
-unzip hw2_1.zip
